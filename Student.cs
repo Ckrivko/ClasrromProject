699dc4879a746a9ecf1b09cf79d09c49df3c1edc
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2021-02-17 22:57:03 +0200

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -19,5 +19,12 @@ namespace ClassroomProject
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Subject { get; set; }
+   
+     public override string ToString()
+        {
+            return $"Student: First Name = {FirstName}, Last Name = {LastName}, Subject = {Subject}";
+
+        }
+    
     }
 }
